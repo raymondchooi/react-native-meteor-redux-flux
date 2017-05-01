59a75b9a2fa215f0052833e86acf912b28d012d2
raymondchooi <raymond_chooi282@hotmail.com>
2017-05-01 03:06:57 +0100

    Add wait for persisting store

--- RNApp/app/index.js
+++ RNApp/app/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { AsyncStorage, View } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
 import { autoRehydrate, persistStore } from 'redux-persist';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import LoginReducers from './login/reducers';
@@ -11,34 +11,48 @@ import SplashScreenContainer from './splashScreen/index';
 import LoginContainer from './login/index';
 import LockerContainer from './locker/index';
 
-const store = createStore(LoginReducers, undefined, autoRehydrate());
-const persistor = persistStore(store, {storage: AsyncStorage});
+const store = compose(autoRehydrate())(createStore)(LoginReducers);
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = { rehydrated: false }
+  }
+
+  componentWillMount(){
+    persistStore(store, {storage: AsyncStorage}, () => {
+      this.setState({ rehydrated: true })
+    })
+  }
+
   render() {
-    return (
-      <Provider store={ store } persistor={ persistor }>
-        <View style={{ flex: 1 }}>
-          <Router>
-            <Scene key='root' hideNavBar={true}>
-              <Scene key="splashScreen"
-                animation='fade'
-                duration='300'
-                initial={true}
-                component={SplashScreenContainer}
-                title="SplashScreen"/>
-              <Scene key="login"
-                duration='300'
-                animation='fade'
-                component={LoginContainer}
-                title="LoginScreen"/>
-              <Scene key="locker"
-                component={LockerContainer}
-                title="Locker"/>
-            </Scene>
-          </Router>
-        </View>
-      </Provider>
-    )
+    if(!this.state.rehydrated){
+      return <View/>
+    } else {
+      return (
+        <Provider store={ store }>
+          <View style={{ flex: 1 }}>
+            <Router>
+              <Scene key='root' hideNavBar={true}>
+                <Scene key="splashScreen"
+                  animation='fade'
+                  duration='300'
+                  initial={true}
+                  component={SplashScreenContainer}
+                  title="SplashScreen"/>
+                <Scene key="login"
+                  duration='300'
+                  animation='fade'
+                  component={LoginContainer}
+                  title="LoginScreen"/>
+                <Scene key="locker"
+                  component={LockerContainer}
+                  title="Locker"/>
+              </Scene>
+            </Router>
+          </View>
+        </Provider>
+      )
+    }
   }
 }
